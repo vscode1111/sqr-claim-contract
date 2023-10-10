71d9d1c0159f648a8299deecad99ff925987fd79
Konstantin Biskub <konstantin.b@msq.ai>
2023-10-10 14:02:57 +0300

    feat: test git

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -10,7 +10,8 @@ export enum CONTRACT_LIST {
 export const TOKENS: Record<CONTRACT_LIST, DeployNetworks> = {
   SQR_CLAIM: {
     // bnb: '0x06Cd8543cD076807d4F2B3bF67069015206FA9e9', //My
-    bnb: '0x1635A6Bc428F4912c32088313A279605198EA29d', //My2
+    // bnb: '0x1635A6Bc428F4912c32088313A279605198EA29d', //My2
+    bnb: '0xe25002544E53a63f2b3003Ec14AA4a4a18472207', //My3
     polygon: '',
   },
 };
